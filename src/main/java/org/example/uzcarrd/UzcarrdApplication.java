fdc8dbd156422bb86011a36bc2021e296e2a607e
ozod <abdujalilovozod95@gmail.com>
2024-04-09 12:36:05 +0500

    first commit

---
+++ src/main/java/org/example/uzcarrd/UzcarrdApplication.java
@@ -0,0 +1,12 @@
+package org.example.uzcarrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzcarrdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UzcarrdApplication.class, args);
+        //HTGGJG
+    }
+}
---
+++ src/test/java/org/example/uzcarrd/UzcarrdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.uzcarrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzcarrdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
